930ad8085cb37b0fdd04b0adb1504b1b45cf24ec
jacobklitzke <jacobklitzke@gmail.com>
2017-01-09 11:27:34 -0600

    IRRecord Test

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var express = require('express')
+var IRRecord = require('infrared').irrecord;
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  var irrecord = new IRRecord();
+  irrecord.on('stdout', function(data) {
+    console.log(data);
+  });
+  irrecord.on('stderr', function(data) {
+    console.log(data);
+  });
+  irrecord.on('exit', function() {
+    // handle exit event
+  });
+  irrecord.start('remote', {disable_namespace: true});
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+/*
+setTimeout(function() {
+  irrecord.quit();
+}, 5000);
+*/
