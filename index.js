da2812f8e299deef589049218e39fe91d38c3e0e
jacobklitzke <jacobklitzke@gmail.com>
2017-01-11 14:45:52 -0600

    Added Enter Key

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get('/', function (req, res) {
   //TODO Before running this command, lirc must be stopped with "sudo systemctl stop lirc"
   //TODO There should also be a way to name the remote control before starting so 'remote' below is replaced with a name variable.
   irrecord.start('remote', {disable_namespace: true});
+  irrecord.write('Enter');
 })
 
 app.listen(3000, function () {
