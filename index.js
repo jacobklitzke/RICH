13e8acfda91d6fe86e04e24be276ef8d25e45655
mrshiz37 <kansas11213@gmail.com>
2017-01-25 16:05:54 -0600

    Added Home page

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-var express = require('express')
+var express = require('express');
 var IRRecord = require('infrared').irrecord;
 var exec = require('child_process').exec;
-var app = express()
+var app = express();
 
-app.use(express.static('angular'))
+app.use(express.static('angular'));
 app.get('/addRemote', function (req, res) {
   require('./controllers/addRemote').get(req, res);
-})
+});
+
+
+app.get ('/Home', function (req, res) {
+  require('./controllers/Home').get(req,res);
+});
+
+
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
 
 /*
 setTimeout(function() {
