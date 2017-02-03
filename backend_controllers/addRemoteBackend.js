18f0a057dc2522bb52276d7f66d69a10acc611e7
Jacob Klitzke <jacobklitzke@gmail.com>
2017-02-03 17:59:49 -0600

    Added addRemote view and backend functionality

--- backend_controllers/addRemoteBackend.js
+++ backend_controllers/addRemoteBackend.js
@@ -1,5 +1,20 @@
 var path = require('path');
+var fs = require('fs');
 exports.get = function(req, res)
 {
+  console.log(req.query.selected);
   res.send('Hello World!');
+};
+
+exports.getFiles = function(req, res)
+{
+  var fileArr = [];
+  fs.readdir('files', function(err, files) {
+    for(var i = 0; i < files.length; i++) {
+      fileArr.push({
+        fileName: files[i]
+      });
+    }
+    res.json(fileArr);
+  });
 };
--- index.js
+++ index.js
@@ -5,13 +5,18 @@ var app = express();
 
 app.use(express.static('angular'));
 app.use(express.static('backend_controllers'));
+app.use(express.static('files'));
+app.use(express.static('views'));
 app.get('/addRemote', function (req, res) {
   require('./controllers/addRemote').get(req, res);
 });
 app.get('/addRemoteBackend', function (req, res) {
   require('./backend_controllers/addRemoteBackend').get(req, res);
 });
-app.get ('/Home', function (req, res) {
+app.get('/addRemoteBackend/getFiles', function (req, res) {
+  require('./backend_controllers/addRemoteBackend').getFiles(req, res);
+});
+app.get('/Home', function (req, res) {
   require('./controllers/Home').get(req,res);
 });
 app.listen(3000, function () {
