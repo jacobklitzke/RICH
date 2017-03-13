a727962e77ad7d5fa82d1746ce5315c3d5a370c4
jacobklitzke <jacobklitzke@gmail.com>
2017-03-12 22:28:58 -0500

    Updated getRemoteButtons function

--- backend_controllers/recordRemoteBackend.js
+++ backend_controllers/recordRemoteBackend.js
@@ -153,7 +153,7 @@ exports.postRecordData = function(req, res) {
 
 exports.getRemoteButtons = function(req, res) {
     var buttons = JSON.parse(fs.readFileSync('user_files/remote_buttons.json'));
-    res.json('user_files/remote_buttons.json');
+    res.json(buttons);
 };
 
 exports.quitIRRecord = function(req, res) {
